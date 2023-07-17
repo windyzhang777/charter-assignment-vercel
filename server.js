661ecb32f7c42037b7e655d2bed26b341a4e151b
Windy Zhang <windy.zhang777@hotmail.com>
2023-07-16 19:47:33 -0500

    moved transaction data

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router(
-  path.join(__dirname, "data/transaction.json")
+  path.join(__dirname, "src/data/transaction.json")
 );
 const middlewares = jsonServer.defaults();
 const PORT = process.env.PORT || 8000;
